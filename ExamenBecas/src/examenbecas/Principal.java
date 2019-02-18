1f72defdb8e609fc8a27ee4f8539bf6c6c54e437
admin <admin@Win7_1TB>
2019-02-18 09:02:02 +0100

    Codificado entero el main

--- ExamenBecas/src/examenbecas/Principal.java
+++ ExamenBecas/src/examenbecas/Principal.java
@@ -2,83 +2,90 @@ package examenbecas;
 import java.util.*;
 
 public class Principal {
-    static final int MAXBECAS=20;
+	static final int MAXBECAS=20;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		
-		Beca becas[]=new Beca[MAXBECAS];
-		int numBecas=0;
-		
-		int menu;
+		String ciudad;
+		Vector<Beca> becas=new Vector<Beca>(MAXBECAS);
+		int tipo, menu;
+		int nota;
 		
 		// Se añaden tantas solicitudes como desee el usuario
 		
-		
 		do{
-			
-			
 				System.out.println("Dni del solicitante: ");
 				String dni=sc.nextLine();
 				
-				int encontrado=buscarDni(dni, becas, numBecas);
+				int encontrado=becas.indexOf(new Beca(dni));
 				
 				if(encontrado == -1){
 					System.out.println("Renta percápita del solicitante: ");
 					double renta=sc.nextDouble();
-					sc.nextLine();
-					
-									
-					becas[numBecas]=new Beca(dni, renta);
-					numBecas++;
-						
+					do {
+						System.out.println("¿Qué tipo de beca desea? (1-FP, 2-Libros): ");
+						tipo=sc.nextInt();
+					}while(tipo!=1&&tipo!=2);
+					if(tipo==1) {
+						sc.nextLine();
+						System.out.println("Ciudad: ");
+						ciudad=sc.nextLine();
+						becas.add(new BecaFP(dni, renta, ciudad));
+					}
+					else {
+						System.out.println("Nota: ");
+						nota=sc.nextInt();
+						becas.add(new BecaLibro(dni, renta, nota));
+					}
 				}
 				else
 					System.out.println("Este alumno ya ha hecho una solicitud");
 				
-				if (numBecas==MAXBECAS)
+				if (becas.size()==MAXBECAS)
 					break;
 				System.out.println("Añadir otra solicitud:1. Si\n2. No");
 				menu=sc.nextInt();
-			
+				sc.nextLine();
 		}while(menu==1);
 		
-		System.out.println("Damos puntuación a las becas*************");
+		System.out.println("\nDamos puntuación a las becas*************");
 		
-		for(int cont=0; cont < numBecas; cont++){
-			becas[cont].darPuntuacion();
-			System.out.println(becas[cont].toString());	
+		for(int cont=0; cont < becas.size(); cont++){
+			becas.get(cont).darPuntuacion();
+			System.out.println(becas.get(cont).toString());	
 		}
 		
-		System.out.println("Ordenamos las becas por puntos, y a puntos iguales por menos renta***********");
+		System.out.println("\nOrdenamos las becas por puntos, y a puntos iguales por menos renta***********");
 		int j,k;
 		Beca aux;
-		for(j = 1; j < numBecas; j++)
-			for(k = 0; k < numBecas-1; k++){
+		for(j = 1; j < becas.size(); j++)
+			for(k = 0; k < becas.size()-1; k++){
 				
-				if(becas[k].getPuntuacion() < becas[k+1].getPuntuacion()  || 
-				   becas[k].getPuntuacion() == becas[k+1].getPuntuacion() && becas[k].getRenta() > becas[k+1].getRenta()){
+				if(becas.get(k).getPuntuacion() < becas.get(k+1).getPuntuacion()  || 
+						becas.get(k).getPuntuacion() == becas.get(k+1).getPuntuacion() && becas.get(k).getRenta() > becas.get(k+1).getRenta()){
 					
-					aux=becas[k];
-					becas[k]=becas[k+1];
-					becas[k+1]= aux;				
+					aux=becas.get(k);
+					becas.set(k, becas.get(k+1));
+					becas.set(k+1, aux);				
 				}
 				
 			}
 		
 		/* Mostramos las puntuaciones de todas las becas */
-		for(int cont=0; cont < numBecas; cont++)
-			System.out.println(becas[cont]);
-						
-	}
-
-	
-	public static int buscarDni(String dni, Beca becas[],int tam){
-		
-		for(int cont=0; cont < tam;cont++)
-			if(becas[cont].getDnialumno().equals(dni))
-				return cont;
-		return -1;
+		for(int cont=0; cont < becas.size(); cont++)
+			System.out.println(becas.get(cont));
+					
+		System.out.println("\nMostramos únicamente las becas de FP y eliminamos las que tienen 0 puntos****");
+		// Elimino del array las becas de FP con 0 puntos
+		for(int cont=0; cont < becas.size(); cont++) {
+			if(becas.get(cont) instanceof BecaFP&&becas.get(cont).getPuntuacion()==0) {
+				becas.remove(cont);
+				cont--;
+			}
+		}
+		// Muestro por pantalla las becas de FP
+		for(int cont=0; cont < becas.size(); cont++)
+			if(becas.get(cont) instanceof BecaFP)
+				System.out.println(becas.get(cont));
 	}
 	
 	
